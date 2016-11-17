abe06c2d9ce19ce6048c3fd7293c33693875d530
hamletbon <halin@hkis.edu.hk>
2016-11-17 14:33:38 +0800

    initCommit

---
+++ Array2DExercises.java
@@ -0,0 +1,69 @@
+
+/**
+ * Write a description of class Array2DExercises here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Array2DExercises
+{
+    public static int max(int[][] a)
+    {
+        //to be completed
+        return 0;
+    }
+    
+    public static int rowSum(int[][] a, int x)
+    {
+        //to be completed
+        return 0;
+    }    
+    public static int columnSum(int[][] a, int x)
+    {
+        //to be completed
+        return 0;
+    } 
+    public static int[] allRowSums(int[][] a)
+    {
+        //to be completed
+        return new int[1];
+    }    
+    public static boolean isRowMagic(int[][] a)
+    {
+        //to be completed
+        return true;
+    }       
+    public static boolean isColumnMagic(int[][] a)
+
+    {
+        //to be completed
+        return true;
+    }           
+    public static boolean isSquare(int[][] a)
+
+    {
+        //to be completed
+        return true;
+    }           
+    public static boolean isMagic(int[][] a)
+   
+    {
+        //to be completed
+        return true;
+    }       
+    public static boolean isLatin(int[][] a)
+
+    {
+        //to be completed
+        return true;
+    }          
+    public static boolean isSequence(int[][] a)
+   
+    {
+        //to be completed
+        return true;
+    }       
+}
+
+ 
+
